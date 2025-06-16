f51d25ca79545fbc02b33c3c4ea7bd785c99f9ca
김준영 <1090jun@naver.com>
2025-06-16 10:59:25 +0900

    deploy_test_v1

--- backend.py
+++ backend.py
@@ -70,4 +70,4 @@ async def chat_endpoint(request : ChatRequest):
 # 프로그램 실행
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app", host="0.0.0.0", port=8000)
